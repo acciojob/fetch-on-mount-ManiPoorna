fc511ae139e28d147181d00502e5b05238ee4cd7
Mani Poorna <ndmprao2001@gmail.com>
2023-10-20 10:23:52 +0530

    V 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+import axios from "axios";
 
 const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response)
+    .then(data => setData(data.data))
+  }, [])
+  console.log(data)
   return (
     <div>
-        {/* Do not remove the main div */}
+      {
+        data && data.map((data,i) => (
+          <div key={i}>
+            <h2>{data.title}</h2>
+            <p>{data.body }</p>
+          </div>
+        ))
+      }
     </div>
   )
 }
